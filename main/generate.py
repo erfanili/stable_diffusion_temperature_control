5d6addb4d5b8b4a19488a5fc6467012bafeaab7f
erfanili <er.esmaili2060@gmail.com>
2024-09-05 18:36:55 -0400

    sd1_5 ready

--- main/generate.py
+++ main/generate.py
@@ -1,47 +1,30 @@
+
+
+from utils.load_save_utils import *
+
+import random
 import torch
 import os
-from diffusers import StableDiffusionPipeline
-from sd1_5.pipeline_stable_diffusion_x import StableDiffusionPipelineX
-from diffusers.schedulers.scheduling_ddim import DDIMScheduler
-# from modules.unet_2d_condition_x import UNet2DConditionModel
-import pickle
-from sd1_5.attention_processor_x import AttnProcessorX
-from diffusers.models.unets.unet_2d_condition import UNet2DConditionModel
-from utils.load_save_utils import *
-import random
 
 
 
-model_id = "runwayml/stable-diffusion-v1-5"
-model_class = StableDiffusionPipelineX
-device = 'cuda:3'
-time_step_spacing = 'linspace'
-scheduler_class = DDIMScheduler
-generation_output_dir = './generation_outputs'
-prompt_dir = './prompt_files'
 #enter prompt_file name
-prompt_file_name = 'try.txt'
-num_seeds_per_prompt = 3
 num_inference_steps = 50
+prompt_file_name = 'try.txt'
+prompt_dir = './prompt_files'
+generation_output_dir = './generation_outputs'
+num_seeds_per_prompt = 3
+device = 'cuda:3'
 
 
-pipe = load_model(model_class=model_class, model_id = model_id, device = device)
-scheduler = scheduler_class.from_config(pipe.scheduler.config)
-pipe.scheduler.config.timestep_spacing = time_step_spacing
 
 
-def get_prompts_dict(dir:str, file_name = None):
-    if file_name is None:
-        print('error: enter prompts file name')
-    else:
-        file_path = os.path.join(dir,file_name)
-        with open(file_path,'r') as f:
-            lines = f.readlines()
-            lines = {idx:line.strip() for idx,line in enumerate(lines)}
-        
-        return lines
+models = ['sd1_5', 'sd1_5x', 'pixart']
+pipe = load_model(model_name = 'sd1_5x', device = device)
 
-prompts_dict = get_prompts_dict(dir = prompt_dir, file_name=prompt_file_name)
+
+
+prompts_dict = get_prompts_dict(directory = prompt_dir, file_name=prompt_file_name)
 attn_dicts_save_dir = os.path.join(generation_output_dir,prompt_file_name.replace('.txt', ''),'attn_dicts')
 images_save_dir = os.path.join(generation_output_dir,prompt_file_name.replace('.txt', ''),'images')
 
@@ -55,8 +38,8 @@ for idx, prompt in prompts_dict.items():
             image = pipe(prompt = prompt, num_inference_steps = num_inference_steps , generator = generator).images[0]
             attns_dict = pipe.attn_fetch_x.storage_x
             
-            save_dict(dict = attns_dict, dir = attn_dicts_save_dir, file_name = f'{idx}_{seed}')
-            save_image(image = image, dir = images_save_dir, file_name = f'{idx}_{seed}')
+            save_dict(data = attns_dict, directory = attn_dicts_save_dir, file_name = f'{idx}_{seed}')
+            save_image(image = image, directory= images_save_dir, file_name = f'{idx}_{seed}')
 
 
 # # 
--- main/utils/load_save_utils.py
+++ main/utils/load_save_utils.py
@@ -1,3 +1,11 @@
+from diffusers import StableDiffusionPipeline
+from diffusers.schedulers.scheduling_ddim import DDIMScheduler
+from transformers import T5EncoderModel
+from diffusers import PixArtAlphaPipeline
+
+from sd1_5.pipeline_stable_diffusion_x import StableDiffusionPipelineX
+
+
 
 from scipy import spatial
 import torch 
@@ -17,16 +25,16 @@ from PIL import Image
 
 
 ####dicts
-def save_dict(dict: dict,dir: str, file_name: str):
-    os.makedirs(dir, exist_ok = True)
-    file_path = os.path.join(dir,file_name)
+def save_dict(data: dict,directory: str, file_name: str):
+    os.makedirs(directory, exist_ok = True)
+    file_path = os.path.join(directory,file_name)
     with open (f'{file_path}', 'wb') as f:
-        pickle.dump(dict, f)
+        pickle.dump(data, f)
         
         
 
-def load_dict(dir:str, file_name:str):
-    file_path = os.path.join(dir,file_name)
+def load_dict(directory:str, file_name:str):
+    file_path = os.path.join(directory,file_name)
     if not os.path.exists(file_path):
         print("File does not exist.")
     else:
@@ -34,7 +42,17 @@ def load_dict(dir:str, file_name:str):
             data = pickle.load(f)
         return data
     
-    
+
+def get_prompts_dict(directory:str, file_name = None):
+    if file_name is None:
+        print('error: enter prompts file name')
+    else:
+        file_path = os.path.join(directory,file_name)
+        with open(file_path,'r') as f:
+            lines = f.readlines()
+            lines = {idx:line.strip() for idx,line in enumerate(lines)}
+        
+        return lines
     
 ###image
 
@@ -46,18 +64,18 @@ def format_time():
     formatted_str = dt.strftime("%H:%M:%S")
     return formatted_str
 
-def save_image(image,dir,file_name):
+def save_image(image,directory,file_name):
 
-    os.makedirs(dir, exist_ok = True)
-    file_path = os.path.join(dir,file_name)
+    os.makedirs(directory, exist_ok = True)
+    file_path = os.path.join(directory,file_name)
     image.save(f'{file_path}_{format_time()}.jpg')
     
     
-def images_to_gif(dir, output_path, duration=200, loop=1000):
-    os.makedirs(dir, exist_ok=True)
-    image_paths = os.listdir(dir)
+def images_to_gif(directory, output_path, duration=200, loop=1000):
+    os.makedirectorys(directory, exist_ok=True)
+    image_paths = os.listdirectory(directory)
     num_timesteps = len(image_paths)
-    sorted_image_paths = [os.path.join(dir,f'{i}.jpg') for i in range(num_timesteps)]
+    sorted_image_paths = [os.path.join(directory,f'{i}.jpg') for i in range(num_timesteps)]
     images = [Image.open(image) for image in sorted_image_paths]
     images[0].save(
         output_path,
@@ -72,12 +90,39 @@ def images_to_gif(dir, output_path, duration=200, loop=1000):
 ####model
 
 
-def load_model(model_class, device, model_id = None):
+def load_model(model_name,device):
+    if model_name== 'sd1_5':
+        model_class = StableDiffusionPipeline
+        scheduler_class = DDIMScheduler
+        model_id = "runwayml/stable-diffusion-v1-5"
+        time_step_spacing = 'linspace'
+        model = model_class.from_pretrained(pretrained_model_name_or_path=model_id)
+        scheduler = scheduler_class.from_config(model.scheduler.config)
+        model.scheduler = scheduler
+        model.scheduler.config.timestep_spacing = time_step_spacing
+        model.to(device)
+    elif model_name == 'sd1_5x':
+        model_class = StableDiffusionPipelineX
+        scheduler_class = DDIMScheduler
+        model_id = "runwayml/stable-diffusion-v1-5"
+        time_step_spacing = 'linspace'
+        model = model_class.from_pretrained(pretrained_model_name_or_path=model_id)
+        scheduler = scheduler_class.from_config(model.scheduler.config)
+        model.scheduler = scheduler
+        model.scheduler.config.timestep_spacing = time_step_spacing
+        model.to(device)
     
-    if model_id is not None:
+    elif model_name == 'pixart':
+        model_class = PixArtAlphaPipeline
+        model_id = "PixArt-alpha/PixArt-XL-2-1024-MS"
         model = model_class.from_pretrained(pretrained_model_name_or_path=model_id)
         model.to(device)
-
+    
+    else:
+        print('model not accepted')
+        exit()
+    
+        
     return model
 
 
