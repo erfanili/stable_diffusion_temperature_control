95693f016ac0d20ab9e93aebb2b1b611bd51121a
erfanili <er.esmaili2060@gmail.com>
2024-09-06 18:39:32 -0400

    processors unified

--- code_dump/word_embeddings/pixart_one.py
+++
@@ -1,5 +0,0 @@
-from transformers import T5EncoderModel
-from diffusers import PixArtAlphaPipeline
-import torch
-pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS", torch_dtype=torch.float16)
-pipe.to("cuda:3")
