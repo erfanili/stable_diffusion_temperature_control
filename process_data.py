4183aa00f04be6e533859d2a614b62241eeff262
erfanili <er.esmaili2060@gmail.com>
2024-09-10 15:34:08 -0400

    indices uopdated

--- process_data.py
+++ process_data.py
@@ -9,40 +9,62 @@ process_prompt_files = True
 if process_prompt_files:
 
     txt_directory = './prompt_files/txt/'
-    json_directory = './prompt_files/json/'
+    json_words_directory = './prompt_files/json/words/'
+    json_idx_directory = './prompt_files/json/indices/'
     file_names = get_file_names(directory=txt_directory)
     for name in file_names:
-        file_data = {}
+        obj_words_in_file = {}
+        obj_idx_in_file ={}
         prompt_list = get_prompt_list_by_line(directory=txt_directory, file_name=name)
         for idx, prompt in enumerate(prompt_list):
+            prompt_words = {}
+            prompt_idx ={}
             words = prompt.split(' ')
-            if 'and' not in words:
-                print(name, idx)
-            else:
-                index_of_and = words.index('and')
-                if words[0] in ('a' , 'an'):
-                    adj1 = words[1]     
-                    noun1 = (' ').join(words[2:index_of_and])
-                    if words[index_of_and+1] in ('a' , 'an'):
-                        adj2 = words[index_of_and+2]
-                        noun2 = (' ').join(words[index_of_and+3:]).strip('\n. ')
-                    else:
-                        adj2 = words[index_of_and+1]
-                        noun2 = (' ').join(words[index_of_and+2:]).strip('\n. ')
+            index_of_and = words.index('and')
+            if words[0] in ('a' , 'an'):
+                adj1_idx = 1
+                adj1 = words[adj1_idx]
+                noun1_idx = list(range(2,index_of_and))   
+                noun1 = (' ').join([words[_] for _ in noun1_idx])
+                if words[index_of_and+1] in ('a' , 'an'):
+                    adj2_idx = index_of_and+2
+                    adj2 = words[adj2_idx]
+                    noun2_idx = list(range(index_of_and+3,len(words)))
+                    noun2 = (' ').join(words[_] for _ in noun2_idx).strip('\n. ')
+                    if len(noun2_idx) >1 :
+                        print(name,idx)
                 else:
-                    adj1 = words[0]     
-                    noun1 = (' ').join(words[1:index_of_and])
-                    if words[index_of_and+1] in ('a', 'an'):
-                        adj2 = words[index_of_and+2]
-                        noun2 = (' ').join(words[index_of_and+3:]).strip('\n. ')
-                    else:
-                        adj2 = words[index_of_and+1]
-                        noun2 = (' ').join(words[index_of_and+2:]).strip('\n. ')
-                    
-                prompt_data = {'adj1': adj1, 'noun1': noun1, 'adj2': adj2, 'noun2': noun2}
-            file_data[idx] = prompt_data
-        save_json(directory=json_directory, file_name=name,data=file_data)
-    
+                    adj2 = words[index_of_and+1]
+                    noun2_idx = list(range(index_of_and+2,len(words)))
+                    if len(noun2_idx) >1 :
+                        print(name,idx)
+                    noun2 = (' ').join(words[_] for _ in noun2_idx).strip('\n. ')
+            else:
+                adj1 = words[0]     
+                noun1 = (' ').join(words[1:index_of_and])
+                if words[index_of_and+1] in ('a', 'an'):
+                    adj2_idx = index_of_and+1
+                    adj2 = words[index_of_and+2]
+                    noun2_idx = list(range(index_of_and+3, len(words)))
+                    noun2 = (' ').join(words[_] for _ in noun2_idx).strip('\n. ')
+                    if len(noun2_idx) >1 :
+                        print(name,idx)
+                else:
+                    adj2_idx = index_of_and+1
+                    adj2 = words[adj2_idx]
+                    noun2_idx = list(range(index_of_and+2,len(words)))
+                    noun2 = (' ').join(words[_] for _ in noun2_idx).strip('\n. ')
+                    if len(noun2_idx) >1 :
+                        print(name,idx)
+                
+            prompt_words = {'adj1': adj1, 'noun1': noun1, 'adj2': adj2, 'noun2': noun2}
+            prompt_idx = {'adj1': adj1_idx, 'noun1': noun1_idx, 'adj2': adj2_idx, 'noun2': noun2_idx}
+            obj_words_in_file[idx] = prompt_words
+            obj_idx_in_file[f'obj_{idx}_1'] = noun1_idx
+            obj_idx_in_file[f'obj_{idx}_2'] = noun2_idx
+        save_json(directory=json_words_directory, file_name=name,data=obj_words_in_file)
+        save_json(directory=json_idx_directory, file_name=name,data=obj_idx_in_file)
+
     
     
     
